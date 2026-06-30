dacc70647e944f9bb898d9d23eaa74c1d020f590
Pro-Frontier-Academy <smbaqirlecture@gmail.com>
2026-06-30 22:29:43 +0500

    Update video-links.js

--- videos/module-3-videos/video-links.js
+++ videos/module-3-videos/video-links.js
@@ -15,4 +15,10 @@
 // the page will show a friendly "coming soon" message instead.
 // ============================================================
 
-const MODULE_3_VIDEOS = [];
+const MODULE_3_VIDEOS = [
+  {
+    "part": 1,
+    "title": "Session 3.1",
+    "url": "https://drive.google.com/file/d/1vmu7DbSJZM1rmXJugCengBJuTqanNknn/view?usp=sharing"
+  } 
+];
